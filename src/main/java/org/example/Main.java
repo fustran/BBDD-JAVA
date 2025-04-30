7e6501ec4ed27e3e5164c68f481eabb6e45bdb21
fustran <ifussim@outlook.com>
2025-04-30 21:12:15 +0200

    BBDD-java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static java.lang.System.*;
+import static java.sql.DriverManager.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Connection bd = conectar();
+        desconectar(bd);
+
+    }
+
+    public static Connection conectar() {
+        Connection conexion;
+        String host = "jdbc:mariadb://localhost:3307/";
+        String user = "root";
+        String password = "";
+        String bd = "Instituto";
+
+        try {
+            conexion = getConnection(host + bd,user,password);
+            out.println("Conexión realizada con éxito!");
+        } catch (SQLException e) {
+            out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return conexion;
+    }
+
+    public static void desconectar(Connection conexion) {
+        try {
+            conexion.close();
+            out.println("Desconexión realizada con éxito");
+        } catch (SQLException e) {
+            out.println("Error de desconexión");
+            throw new RuntimeException(e);
+        }
+    }
+}
